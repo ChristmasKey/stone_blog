92ae3eae2bdf6cd22d27a21f6363e44abc68aa22
ChristmasKey <Jiananst@qq.com>
2021-11-30 23:02:35 +0800

    [环境搭建]初始化项目环境 1.修改配置文件application.properties为application.yml 2.在pom文件中预备需要的依赖坐标，放在注释里

---
+++ src/main/java/com/djn/StoneBlogApplication.java
@@ -0,0 +1,13 @@
+package com.djn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoneBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/djn/StoneBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoneBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
