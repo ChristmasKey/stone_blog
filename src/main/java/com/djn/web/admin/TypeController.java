a0865787930df8a8846aecef57670688532a5ac5
ChristmasKey <Jiananst@qq.com>
2021-12-06 01:03:02 +0800

    [项目开发]分类管理功能实现 1.修改分类 2.删除分类

--- src/main/java/com/djn/web/admin/TypeController.java
+++ src/main/java/com/djn/web/admin/TypeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -41,6 +42,12 @@ public class TypeController {
         return "admin/addType";
     }
 
+    @GetMapping("/types/{id}/input")
+    public String editType(@PathVariable Long id, Model model) {
+        model.addAttribute("type", typeService.getType(id));
+        return "admin/addType";
+    }
+
     @PostMapping("/types")
     public String post(@Valid Type type, BindingResult result, RedirectAttributes attributes) {
 
@@ -58,10 +65,37 @@ public class TypeController {
         Type t = typeService.saveType(type);
 
         if (t == null) {
-            attributes.addFlashAttribute("message", "操作失败");
+            attributes.addFlashAttribute("message", "添加失败");
         } else {
             attributes.addFlashAttribute("message", "添加成功");
         }
+        return "redirect:/admin/types";
+    }
+
+    @PostMapping("/types/{id}")
+    public String editPost(@Valid Type type, BindingResult result, @PathVariable Long id, RedirectAttributes attributes) {
+        Type existType = typeService.getTypeByName(type.getName());
+        if (existType != null) {
+            result.rejectValue("name", "nameError", "不能添加重复的分类");
+        }
+
+        if (result.hasErrors()) {
+            return "admin/addType";
+        }
+
+        Type t = typeService.updateType(id, type);
+        if (t == null) {
+            attributes.addFlashAttribute("message", "更新失败");
+        } else {
+            attributes.addFlashAttribute("message", "更新成功");
+        }
+        return "redirect:/admin/types";
+    }
+
+    @GetMapping("/types/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
+        typeService.deleteType(id);
+        attributes.addFlashAttribute("message", "删除成功");
         return "redirect:/admin/types";
     }
 }
